2ac1886c4fdb86b03b98ebf71953a509824de5ab
Servi Huijbregts <346215@student.fontys.nl>
2022-04-16 18:57:52 +0200

    Test with param

--- api/products-get-by-name/index.js
+++ api/products-get-by-name/index.js
@@ -4,8 +4,9 @@ module.exports = async function (context, req) {
   const name = req.params.name;
 
   try {
-    const exercises = data.getExercisesByName(name);
-    context.res.status(200).json(exercises);
+    // const exercises = data.getExercisesByName(name);
+    context.res.status(200).json(name);
+    console.log()
   } catch (error) {
     context.res.status(500).send(error);
   }
