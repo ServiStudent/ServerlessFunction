64ab76ebcba151bad31b2cfb439845d4def06240
Servi Huijbregts <you@example.com>
2022-04-19 14:34:00 +0200

    config file

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -3,6 +3,7 @@ const data = require('../shared/exercise-data');
 module.exports = async function (context, req) {
   try {
     const exercises = data.getExercises();
+    context.res.set('Access-Control-Allow-Origin', '*');
     context.res.status(200).json(exercises);
   } catch (error) {
     context.res.status(500).send(error);
