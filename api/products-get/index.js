270aa9a6b91768cb37761a9faf5395eaabefec29
Servi Huijbregts <you@example.com>
2022-04-19 14:24:43 +0200

    config file

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,24 +1,10 @@
 const data = require('../shared/exercise-data');
 
 module.exports = async function (context, req) {
-
-  context.log('JavaScript HTTP trigger function processed a request.');
-  context.log(context);
-  const exercises = data.getExercises();
-  let res = {
-    // status: 200, /* Defaults to 200 */
-    body: {message: exercises},
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    }
-  };
-  context.done(null, res);
-  // try {
-  //
-  //   context.res.status(200).json(exercises);
-  // } catch (error) {
-  //   context.res.status(500).send(error);
-  // }
+  try {
+    const exercises = data.getExercises();
+    context.res.status(200).json(exercises);
+  } catch (error) {
+    context.res.status(500).send(error);
+  }
 };
