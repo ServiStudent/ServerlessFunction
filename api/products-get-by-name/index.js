4e7d9f03714c8b3ee53d341c65a50c2117f4ec96
Servi Huijbregts <you@example.com>
2022-04-19 14:01:46 +0200

    CORS

--- api/products-get-by-name/index.js
+++ api/products-get-by-name/index.js
@@ -5,10 +5,7 @@ module.exports = async function (context, req) {
 
   try {
     const exercises = data.getExercisesByName(name);
-    context.res.status(200).headers({
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    }).json(exercises);
+    context.res.status(200).json(exercises);
   } catch (error) {
     context.res.status(500).send(error);
   }
--- api/products-get/index.js
+++ api/products-get/index.js
@@ -3,10 +3,7 @@ const data = require('../shared/exercise-data');
 module.exports = async function (context, req) {
   try {
     const exercises = data.getExercises();
-    context.res.status(200).headers({
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    }).json(exercises);
+    context.res.status(200).json(exercises);
   } catch (error) {
     context.res.status(500).send(error);
   }
