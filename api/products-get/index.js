093bb74500ae63356959651c64fb01eb4488b383
Servi Huijbregts <you@example.com>
2022-04-19 14:18:32 +0200

    tryout cors

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,10 +1,24 @@
 const data = require('../shared/exercise-data');
 
 module.exports = async function (context, req) {
-  try {
-    const exercises = data.getExercises();
-    context.res.status(200).json(exercises);
-  } catch (error) {
-    context.res.status(500).send(error);
-  }
+
+  context.log('JavaScript HTTP trigger function processed a request.');
+  context.log(context);
+  const exercises = data.getExercises();
+  let res = {
+    // status: 200, /* Defaults to 200 */
+    body: {message: exercises},
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    }
+  };
+  context.done(null, res);
+  // try {
+  //
+  //   context.res.status(200).json(exercises);
+  // } catch (error) {
+  //   context.res.status(500).send(error);
+  // }
 };
